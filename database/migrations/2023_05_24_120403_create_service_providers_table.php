02c177a1476a167736619c3e6944bec6729a81c1
JEEVANRAWAL <rawaljeevan123@gmail.com>
2024-08-10 14:08:05 +0545

    fix: username and password make nullable of service_providers table

--- database/migrations/2023_05_24_120403_create_service_providers_table.php
+++ database/migrations/2023_05_24_120403_create_service_providers_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('address');
             $table->string('phone_number');
             $table->string('email')->unique();
-            $table->string('user_name');
-            $table->string('password');
+            $table->string('user_name')->nullable();
+            $table->string('password')->nullable();
             $table->timestamps();
         });
     }
